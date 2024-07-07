34daf7cc71330b153b9d73abb041bc5102815202
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-07 21:04:12 +0300

    adding client config

--- client.py
+++ client.py
@@ -1,1 +1,11 @@
-print('hi');
+import yaml
+import os
+
+config_file = os.path.join(os.path.dirname(__file__), '../fullstack-repo/config.yaml')
+
+with open(config_file, 'r') as f:
+    config = yaml.safe_load(f)
+
+server_ip = config.get('ServerIPAddress')
+
+print(f"Connecting to server at IP address: {server_ip}")
